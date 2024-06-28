251b1c955fa39710425db8929c824f3fae944f6b
AdamMohamed2024 <164066092+AdamMohamed2024@users.noreply.github.com>
2024-06-28 15:29:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.write("<h1>Welcome</h1>");
+console.log("Adam");
+alert("Hello");
+var username = prompt("Enter ur name:");
+var name = "Adam";
+console.log(name)
+var age = 9;
+console.log(age);
+var schoolname = "Alson";
+console.log(schoolname);
+console.log("Welcome",username);
+var color = prompt("Enter ur fav color:");
+console.log("Ur fav color is",color);
+var gender = prompt("Enter ur gender:");
+console.log("Ur gender is" ,gender)
+var game = prompt("Enter ur fav game:");
+console.log("Ur fav game is",game);
+var email = prompt("Enter ur email:");
+console.log(email);
+var password = prompt("Enter us password:");
+console.log("Ur password is",password)
